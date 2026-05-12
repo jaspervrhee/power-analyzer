3d0919939881fd32b2c5505cd6f69eceb1df18d4
Jasper van Rhee <jaspervanrhee@gmail.com>
2026-05-12 10:46:24 +0200

    --

--- src/cpp/lld/IEM3250LLD.h
+++ src/cpp/lld/IEM3250LLD.h
@@ -12,7 +12,7 @@ public:
     IEM3250LLD(IIEM3250Communication& comm,
                std::string port     = "/dev/serial0",
                int deviceId         = 1,
-               int baudRate         = 19200,
+               int baudRate         = 38400,
                int timeoutMs        = 2000);
 
     ~IEM3250LLD() override;
